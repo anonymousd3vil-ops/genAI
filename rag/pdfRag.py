11341d53d745ed8b87e1b8a5ad00edaa1be6f0a2
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-09-12 16:00:25 +0530

    added comments in codes

--- rag/pdfRag.py
+++ rag/pdfRag.py
@@ -12,22 +12,26 @@ from pathlib import Path
 
 pdfPath = Path(__file__).parent / "The Ultimate Python Handbook.pdf"
 
-loader = PyPDFLoader(file_path=pdfPath)
+loader = PyPDFLoader(file_path=pdfPath) #load the respective file
 
 docs = loader.load()
 
+#make splitter client this will spilit the complete docs in given chunk size with the overlapping
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=700,
     chunk_overlap=100
 )
 
+#split docs are stored in split_docs
 split_docs = text_splitter.split_documents(documents=docs)
 
+#made an embedded client
 embedder = OllamaEmbeddings(
     model="qwen3-embedding:4b",
     dimensions=1024,
 )
 
+#store complete split_docs in qdrant database running on docker
 # vector_store = QdrantVectorStore.from_documents(
 #     documents=[],
 #     embedding=embedder,
@@ -35,20 +39,24 @@ embedder = OllamaEmbeddings(
 #     url="http://localhost:6333",
 # )
 
+#add the vectors in database
 # vector_store.add_documents(documents=split_docs)
 print("INJECTION DONE")
 
+#giveing datatabse bonter from where we have to take the data
 vector_store = QdrantVectorStore.from_existing_collection(
     embedding=embedder,
     collection_name="learing_langchain",
     url="http://localhost:6333",
 )
 
+#created a chat interface with using OLlama with qwen3 model
 llm = ChatOllama(
     model="qwen3:4b",
     temperature=0,
 )
 
+#ask for user quesy
 query = input("Query >")
 
 # Retrieve relevant documents from Qdrant
@@ -68,7 +76,7 @@ CONTEXT:
 
 """
 
-# Add retrieved chunks to context
+# Add retrieved chunks to context (SYSTEM_PRIOMPT)
 for doc in search_result:
     SYSTEM_PRMPT += "\n\n" + doc.page_content
 
